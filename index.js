d32c45d01c09e6ed9d75f83afffd69656885acd3
Antony <optonny143dr@gmail.com>
2026-02-02 22:32:48 -0300

    fix syntaxes errors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
 const mysql = require("mysql2")
+const cors = require("cors")
 const defs = require("./src/defs")
 const dbConfig = require("./src/configs")
+
 
 const app = express()
 // middlewares
@@ -11,12 +13,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000, (err) => {
-    if (!err) {
-        console.log("API is running")
-    } else {
-        console.log(err.message)
-    }
+app.listen(3000, () => {
+    console.log("API is running")
 })
 let API_AVAILABLE = true
 if (!API_AVAILABLE) {
@@ -29,11 +27,15 @@ const connection = mysql.createPool(dbConfig)
 
 // GET all Scheduling
 app.get("/agendamentos", (req, res) => {
-    connection.query("SELECT * FROM agendamentos", (err, result) => {
+    connection.query("SELECT * FROM agendamentos", (err, rows) => {
         if (!err) {
-            res.status(200).json(defs.response("Sucesso", "Agendamentos encontrados com êxito", result.affectedRows, result))
+            if (rows.length <= 0) {
+                res.status(404).json(defs.response("Erro", "Nenhum agendamento encontrado", 0, null))
+            } else {
+                res.status(200).json(defs.response("Sucesso", "Sucesso ao obter agendamentos", rows.affectedRows, rows))
+            } 
         } else {
-            res.status(404).json(defs.response("Erro", "Agendamento não encontrado.", 0, null))
+            res.status(500).json(defs.response("Erro", err.message, 0, null))
         }
     })
 })
@@ -42,12 +44,18 @@ app.post("/agendamento", (req, res) => {
     const Cliente =  postData.Cliente
     const Data = postData.Data
     const Horario = postData.Horario
-    // query
+// Formato a ser preenchido no JSON
+// {
+//   "Cliente": "José",
+//   "Data": "YYYY-MM-DD",
+//   "Horario": HH:MM:SS"
+// }
     connection.query("INSERT INTO agendamentos (Cliente, Data, Horario) VALUES (?, ?, ?)", [Cliente, Data, Horario], (err, result) => {
-        if (!err) {
-            return res.status(201).json(defs.response("Sucesso", "Cliente agendado com êxito", result.affectedRows, result))
+        if (err) {
+            console.error(err)
+            return res.status(400).json(defs.response("Erro", err.cause, 0, null))
         } else {
-            res.status(400).json(defs.response("Erro", "Falha ao fazer agendamento", 0, null))
+            return res.status(200).json(defs.Scheduling(Cliente, Data, Horario))
         }
     })
 })
